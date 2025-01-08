8a7d88fc87082f352b3265fa168382364b59b299
Aaron Lambert <1637094951113302@mil>
2025-01-08 13:03:47 -0900

    Fixed proportions for TAC

--- Cook_Inlet/Data management.R
+++ Cook_Inlet/Data management.R
@@ -12,7 +12,7 @@ library(lubridate)
 
 # 2024 Catch data from AKFIN
 # Read in the catch data
-CIS_Catch_2024 <- read_xlsx(path  = paste0(getwd(),'/','Inseason_Catch/CIS_data_2024_FINAL_20Nov24.xlsx'))
+CIS_Catch_2024 <- read_xlsx(path  = paste0(getwd(),'/','Inseason Catch Data/CIS_data_2024_FINAL_20Nov24.xlsx'))
 
 # Calculating the 2024 Kenai, Kasilof and Other salmon catch by day
 # Get total catch for the season
@@ -21,9 +21,9 @@ CIS_Catch_2024 <- read_xlsx(path  = paste0(getwd(),'/','Inseason_Catch/CIS_data_
 # Break out Kenai, Kasilof, and other by day
 temp.catch <- CIS_Catch_2024 %>% 
   group_by(OPENER_DATE) %>% 
-  summarise("Kenai Sockeye salmon"= CATCH_PER_DAY[SPECIES == 'Sockeye salmon']*.538, #Kenai proportion
+  summarise("Kenai Sockeye salmon"= CATCH_PER_DAY[SPECIES == 'Sockeye salmon']*.583, #Kenai proportion
             "Kasilof Sockeye salmon" = CATCH_PER_DAY[SPECIES == 'Sockeye salmon']*.24, #Kasilof proportion
-            "Other Sockeye salmon" = CATCH_PER_DAY[SPECIES == 'Sockeye salmon']*(1-0.538-0.24)) %>% 
+            "Other Sockeye salmon" = CATCH_PER_DAY[SPECIES == 'Sockeye salmon']*(1-0.583-0.24)) %>% 
   pivot_longer(cols = c('Kenai Sockeye salmon', 'Kasilof Sockeye salmon','Other Sockeye salmon'), names_to = "SPECIES", values_to = "CATCH_PER_DAY") 
 
 # Subset the original DF for binding
@@ -42,9 +42,9 @@ test2 <- test %>%group_by(OPENER_DATE) %>% arrange(OPENER_DATE) %>% as.data.fram
 TAC.DF<- unique(CIS_Catch_2024[,c("SPECIES","TAC_2024")])
 
 # Calculate the TAC by Sockeye stocks
-Kenai.TAC <- TAC.DF$TAC_2024[TAC.DF$SPECIES == "Sockeye salmon"]*0.538 # From google drive Kenai data
+Kenai.TAC <- TAC.DF$TAC_2024[TAC.DF$SPECIES == "Sockeye salmon"]*0.583 # From google drive Kenai data
 Kasilof.TAC <- TAC.DF$TAC_2024[TAC.DF$SPECIES == "Sockeye salmon"]*0.24 # From google drive Kasilof data
-Other.TAC <- TAC.DF$TAC_2024[TAC.DF$SPECIES == "Sockeye salmon"]*(1-0.538-0.24) # Remaining tac
+Other.TAC <- TAC.DF$TAC_2024[TAC.DF$SPECIES == "Sockeye salmon"]*(1-0.583-0.24) # Remaining tac
 
 # Put TAC by species into df
 Temp.TAC <- data.frame("SPECIES" = c("Kenai Sockeye salmon","Kasilof Sockeye salmon", "Other Sockeye salmon"),
@@ -59,4 +59,4 @@ totTACDF$SPECIES <- factor(totTACDF$SPECIES)
 test3 <-left_join(test2, totTACDF)
 
 # Save as csv
-# write.csv(test3,file = paste0(getwd(),"/Inseason_Catch/2024 EEZ Catch.csv"),row.names = F)
+write.csv(test3,file = paste0(getwd(),"/Inseason Catch Data/2024 EEZ Catch.csv"),row.names = F)
